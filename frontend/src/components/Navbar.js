b6aa66c954d8f8c54db31ff677964c0551ab7060
saiakkala2006 <saiakkala2006@gmail.com>
2026-03-28 19:15:55 +0530

    27/03

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -10,7 +10,6 @@ const Navbar = ({ user, logout, title }) => {
   return (
     <nav className="navbar">
 
-
       <h2>{title || 'Online Exam Portal'}</h2>
       <div className="navbar-right">
         <span>Welcome, {user?.name}</span>
