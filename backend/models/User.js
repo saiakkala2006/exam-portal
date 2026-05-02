dfb0ff5024e816d5038b6b3364511ccec7d14e3e
saiakkala2006 <saiakkala2006@gmail.com>
2026-05-02 11:01:04 +0530

    02/05

--- backend/models/User.js
+++ backend/models/User.js
@@ -12,6 +12,8 @@ const userSchema = new mongoose.Schema({
     required: [true, 'Please add a name'],
     trim: true
   },
+
+  
   email: {
     type: String,
     required: [true, 'Please add an email'],
